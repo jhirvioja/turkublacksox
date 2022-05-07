2bf40fb1e19e060d36640c25b1301d3e4bb35b04
jhirvioja <jhirvioja@gmail.com>
2022-05-07 12:44:33 +0300

    Minified js

--- src/_includes/getup.js
+++ src/_includes/getup.js
@@ -1,17 +1,1 @@
-let mybutton = document.getElementById("myBtn");
-window.onscroll = function () {
-  scrollFunction();
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-document.querySelector("#myBtn").addEventListener("click", () => {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-});
+let mybutton=document.getElementById("myBtn");function scrollFunction(){document.body.scrollTop>500||document.documentElement.scrollTop>500?mybutton.style.display="block":mybutton.style.display="none"}window.onscroll=function(){scrollFunction()},document.querySelector("#myBtn").addEventListener("click",()=>{document.body.scrollTop=0,document.documentElement.scrollTop=0});

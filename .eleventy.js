7702223dce27147a00803f675f30cbca5bd37af3
jhirvioja <jhirvioja@gmail.com>
2022-05-10 19:21:26 +0300

    Added icons, changed webmanifest

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/img");
   eleventyConfig.addPassthroughCopy("src/js");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
+  eleventyConfig.addPassthroughCopy("src/manifest.json");
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   return {
